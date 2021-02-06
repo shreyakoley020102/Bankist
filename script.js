48cd4c46e78ecdb00780b9085dc0c73e11c65d53
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-06 21:35:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -69,10 +69,22 @@ btnScrollto.addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 //Page Navigation
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
+
+//document.querySelectorAll('.nav__link').forEach//(function (el) {
+//  el.addEventListener('click', function (e) {
+//    e.preventDefault();
+//    const id = this.getAttribute('href');
+//    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//  });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  //Matching strategy
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+  }
 });
