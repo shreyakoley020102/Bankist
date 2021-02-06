c4012ad96204148248ef78648455078fe4573440
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-06 17:39:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -58,9 +58,11 @@ btnScrollto.addEventListener('click', function (e) {
   //  s1coords.top + window.pageYOffset
   // );
 
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: 'smooth',
-  });
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
