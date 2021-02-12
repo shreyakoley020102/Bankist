24df266698f7f17346511dce125eb72455b4a53d
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-12 20:16:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -105,5 +105,17 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 // Sticky navigation: Intersection Observer API
-const observer = new IntersectionObserver();
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
