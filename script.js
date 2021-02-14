0841b1f985b6d1c7a4a13c964ed78dc9750734e1
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-14 12:00:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -183,6 +183,8 @@ const loadImg = function (entries, observer) {
   entry.target.addEventListener('load', function () {
     entry.target.classList.remove('lazy-img');
   });
+
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
