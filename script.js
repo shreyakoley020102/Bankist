a645e5f03f34dc3b55705e8fd4f9881cd6654181
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-14 11:11:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -139,3 +139,27 @@ const headerObserver = new IntersectionObserver(stickyNAv, {
 });
 
 headerObserver.observe(header);
+
+/////////////////////////////
+////// Reveal Sections
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
