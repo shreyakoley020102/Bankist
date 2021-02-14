674566958d08448d2b0257140017f79ed751590e
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-14 12:02:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -190,6 +190,7 @@ const loadImg = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
