77d52742f5cb33ca0ff0a885130665522a375f7c
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-15 22:55:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -230,4 +230,11 @@ const nextSlide = function () {
   goToSlide(curSlide);
 };
 
+const prevSlide = function () {
+  curSlide--;
+  goToSlide(curSlide);
+};
+
 btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', prevSlide);
