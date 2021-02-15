ac0605d63c3d6c38713898df351b5d9eb4c1fd3c
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-15 23:02:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -205,9 +205,9 @@ const btnRight = document.querySelector('.slider__btn--right');
 let curSlide = 0;
 const maxSlide = slides.length;
 
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.4) translateX(-800px)';
-slider.style.overflow = 'visible';
+//const slider = document.querySelector('.slider');
+//slider.style.transform = 'scale(0.4) translateX(-800px)';
+//slider.style.overflow = 'visible';
 
 const goToSlide = function (slide) {
   slides.forEach(
