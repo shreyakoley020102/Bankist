77a3b3880b1af838feaa6f6fc2253fad3b3f7bce
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-17 17:48:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -210,6 +210,17 @@ const maxSlide = slides.length;
 //slider.style.transform = 'scale(0.4) translateX(-800px)';
 //slider.style.overflow = 'visible';
 
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+createDots();
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
