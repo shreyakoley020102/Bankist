84db43d83c7e6a9e4b2cbd40096b3bc008c70177
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-17 18:18:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -260,3 +260,9 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'ArrowLeft') prevSlide();
   e.key === 'ArrowRight' && nextSlide();
 });
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    console.log('DOT');
+  }
+});
